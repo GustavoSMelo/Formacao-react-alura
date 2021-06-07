028436f297bed0d66f3edd54740fd39839e8d677
GustavoSMelo <gsantos15569@gmail.com>
2021-06-06 22:32:13 -0300

    update index

--- js-poo/bytebanking/index.js
+++ js-poo/bytebanking/index.js
@@ -1,38 +1,10 @@
-import { Cliente } from './Cliente.js';
-import { ContaCorrente } from './ContaCorrente.js';
+import { Cliente } from './classes/Cliente.js';
+import { ContaCorrente } from './classes/ContaCorrente.js';
+import { ContaPoupanca } from './classes/ContaPoupanca.js';
 
-const client = new Cliente('Gustavo', 3245);
-const client2 = new Cliente('Geovana', 6524);
+const client = new Cliente('Gustavo', 123456);
+const accountC = new ContaCorrente(3214, client);
+const accountP = new ContaPoupanca(0, 3214, client);
 
-const account = new ContaCorrente(4023);
-const account2 = new ContaCorrente(3520);
-
-client.setContaCorrente(account);
-client2.setContaCorrente(account2);
-
-client.show();
-/*
-setTimeout(() => {
-    client.ContaCorrente.deposit(100);
-    client.show();
-    const valorSacadoErrado = client.ContaCorrente.withdraw(150);
-
-    console.log(' ');
-    console.log(valorSacadoErrado);
-    console.log(' ');
-
-    const valorSacado = client.ContaCorrente.withdraw(50);
-    client.show();
-
-    console.log(valorSacado);
-}, 4000);
-*/
-
-client.getContaCorrente().deposit(100);
-client.getContaCorrente().transfer(80, client2);
-
-client.show();
-console.log(' ');
-client2.show();
-
-console.log(`\nNumero de contas abertas: ${ContaCorrente.accountCounter}`)
+console.log(accountC);
+console.log(accountP);
