1efcda005b74e75776db97fec6aef922af6acae6
GustavoSMelo <gsantos15569@gmail.com>
2021-06-07 17:59:39 -0300

    moved to /Contas

--- js-poo/bytebanking/classes/ContaPoupanca.js
+++
@@ -1,7 +0,0 @@
-import { Conta } from "./Conta.js";
-
-export class ContaPoupanca extends Conta {
-    constructor (initialBalance, agency, client) {
-        super(initialBalance, agency, client);
-    }
-}
