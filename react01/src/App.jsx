14477d3c1fcf61cedb0c8884ce4c68ac6d098629
GustavoSMelo <gsantos15569@gmail.com>
2021-06-21 23:14:00 -0300

    new states

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Formulario from './components/formulario';
 import Lista from './components/lista'
 import './style.css'
+import Categoria from './components/categorias'
 
 class App extends Component {
     constructor (props) {
@@ -9,22 +10,48 @@ class App extends Component {
 
         this.state = {
             notes: [],
+            categorias: [],
         }
     }
-    criarNota = (title, annotation) => {
+    criarNota = (title, category , annotation) => {
         this.setState({
-            notes: [...this.state.notes, {title, annotation}]
+            notes: [...this.state.notes, {title, category, annotation}]
         });
 
         console.log(this.state.notes);
+    }
+
+    deletarNota = (arryPosition) => {
+        const newArryNotes = this.state.notes.filter((_, index) => index !== arryPosition);
+
+
+        this.setState({
+            notes: newArryNotes
+        });
+
+    }
+
+    addCategoria = async (categoria) => {
+        await this.setState({
+            categorias: [...this.state.categorias, categoria]
+        });
+
+        console.log(this.state.categorias);
     }
 
     render () {
         return (
             <main className="App">
                 <h1>Notes</h1>
-                <Formulario criarNota={this.criarNota.bind(this)}/>
-                <Lista list={this.state.notes}/>
+                <Categoria addCategoria={this.addCategoria.bind(this)} />
+                <span></span>
+                <Formulario
+                    categorias={this.state.categorias}
+                    criarNota={this.criarNota}
+                />
+                <Lista
+                    list={this.state.notes}
+                    deleteCard={this.deletarNota}/>
             </main>
         );
     }
