288f1c4b3ef8c1a107aa7d1d5ec8d65b41e1f632
GustavoSMelo <gsantos15569@gmail.com>
2021-06-29 21:48:48 -0300

    course of react functions done

---
+++ react02/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Formulario from './components/Formulario'
+import './App.css'
+
+const App = () => {
+  return (
+    <main className="App">
+      <Formulario />
+    </main>
+  )
+}
+
+export default App
