bb4666688bfa650b0cb5373dcd6700d1e5c4bf2a
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-05 21:30:58 -0300

    creating logic to change for next view

--- react02/src/components/FormularioCadastro/index.jsx
+++ react02/src/components/FormularioCadastro/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import DadosUsuario from '../DadosUsuario'
+import React, { useState } from 'react'
 import DadosPessoais from '../DadosPessoais'
-import './style.css'
+import DadosUsuario from '../DadosUsuario'
 import DadosEntrega from '../DadosEntrega'
+import './style.css'
 
 const FormularioCadastro = () => {
+  const [etapaAtual, setEtapaAtual] = useState(0)
+
+  const next = () => {
+    // eslint-disable-next-line no-const-assign
+    const support = etapaAtual + 1
+    setEtapaAtual(support)
+  }
+
+  const formularioRenderizado = (etapa) => {
+    if (etapa === 0) {
+      return <DadosUsuario nextFunction={next} />
+    } else if (etapa === 1) {
+      return <DadosPessoais nextFunction={next} />
+    } else {
+      return <DadosEntrega />
+    }
+  }
+
   return (
     <>
-      <DadosPessoais />
-      <DadosUsuario />
-      <DadosEntrega />
+      {formularioRenderizado(etapaAtual)}
     </>
   )
 }
