9e1cab016e1358c5b7f8e50c5c16be6746dcfcc1
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-07 22:21:27 -0300

    add states in page

--- react02/src/components/DadosUsuario/index.jsx
+++ react02/src/components/DadosUsuario/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, Button } from '@material-ui/core'
 
 // eslint-disable-next-line react/prop-types
 const DadosUsuario = ({ nextFunction }) => {
-  const submitFunction = (event) => {
-    event.preventDefault()
-    nextFunction()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitFunction = () => {
+    nextFunction({ email, password })
   }
+
+  const handleChangeEmail = event => { setEmail(event) }
+  const handleChangePassword = event => { setPassword(event) }
 
   return (
     <form onSubmit={() => submitFunction()}>
-      <TextField className='inputField' label='email' type='text' id='email' required={true} />
-      <TextField className='inputField' label='password' type='password' id='password' required={true} />
+      <TextField className='inputField' label='email' value={email} type='email' id='email' required={true} onChange={event => handleChangeEmail(event.target.value)} />
+      <TextField className='inputField' label='password' valeu={password} type='password' id='password' required={true} onChange={event => handleChangePassword(event.target.value)} />
       <Button type='submit' variant='contained' color='primary'>Continuar... </Button>
     </form>
   )
