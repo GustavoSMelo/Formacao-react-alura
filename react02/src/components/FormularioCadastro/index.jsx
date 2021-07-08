5039a6403f19f6303aeabbb0c2d1c541a9aa66c0
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-07 22:22:05 -0300

    create a function to get all datas and send log for us

--- react02/src/components/FormularioCadastro/index.jsx
+++ react02/src/components/FormularioCadastro/index.jsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import DadosPessoais from '../DadosPessoais'
 import DadosUsuario from '../DadosUsuario'
 import DadosEntrega from '../DadosEntrega'
 import './style.css'
+import { Typography } from '@material-ui/core'
 
 const FormularioCadastro = () => {
   const [etapaAtual, setEtapaAtual] = useState(0)
+  const [datas, setDatas] = useState([])
 
-  const next = () => {
+  useEffect(() => {
+    console.log(datas)
+  })
+
+  const next = info => {
+    setDatas([...datas, info])
     // eslint-disable-next-line no-const-assign
+    console.log(info)
+    console.log(datas)
     const support = etapaAtual + 1
     setEtapaAtual(support)
   }
 
-  const formularioRenderizado = (etapa) => {
-    if (etapa === 0) {
-      return <DadosUsuario nextFunction={next} />
-    } else if (etapa === 1) {
-      return <DadosPessoais nextFunction={next} />
-    } else {
-      return <DadosEntrega />
-    }
-  }
+  const views = [
+    <DadosUsuario key={'form-view-1'} nextFunction={next} />,
+    <DadosPessoais key={'form-view-2'} nextFunction={next} />,
+    <DadosEntrega key={'form-view-3'} nextFunction={next}/>,
+    <Typography key={'form-view-4'} variant='h5' style={{ textAlign: 'center' }}>Obrigado pela sua compra</Typography>
+  ]
 
   return (
     <>
-      {formularioRenderizado(etapaAtual)}
+      <h1 className='title-index-form'>Formulario de cadastro</h1>
+      {views[etapaAtual]}
     </>
   )
 }
