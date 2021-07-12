0aae58c3f1674b452de33c2044b57b90f31e9d9c
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-12 14:46:43 -0300

    new routes in our project

--- react03/src/routes/routes.jsx
+++ react03/src/routes/routes.jsx
@@ -6,6 +6,7 @@ import Sobre from '../views/Sobre'
 import Navbar from '../components/navbar'
 import Postagens from '../views/Postagens'
 import Post from '../views/Post'
+import Categoria from '../views/Categoria'
 
 const Routes = () => {
     return (
@@ -16,6 +17,7 @@ const Routes = () => {
                 <Route exact path='/sobre' component={Sobre} />
                 <Route exact path='/posts' component={Postagens} />
                 <Route exact path='/posts/:id' component={Post} />
+                <Route exact path='/categoria/:id' component={Categoria} />
 
                 <Route component={Pagina404}/>
             </Switch>
