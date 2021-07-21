b3a248ff6e706a6a432e5f957146a7455f6044a9
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-20 22:10:18 -0300

    colors of themes

--- react04/src/themes/variables.js
+++ react04/src/themes/variables.js
@@ -1,3 +1,7 @@
 export const corPrimaria = '#41d3be';
 export const backgroundLight = '#f1f1f1';
 export const txtBackgroundLight = '#808080';
+export const lightContent = '#fff';
+export const darkContent = '#363537';
+export const backgroundDark = '#5c5b5e';
+export const txtBackgroundDark = '#fafafa';
