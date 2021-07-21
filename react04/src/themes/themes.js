712763b6830e46fe7d26900863f57341428937fd
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-20 22:10:32 -0300

    colors of themes

---
+++ react04/src/themes/themes.js
@@ -0,0 +1,20 @@
+import {
+    backgroundDark,
+    backgroundLight,
+    darkContent,
+    lightContent,
+    txtBackgroundDark,
+    txtBackgroundLight
+} from './variables';
+
+export const LightTheme = {
+    body: backgroundLight,
+    inside: lightContent,
+    text: txtBackgroundLight
+}
+
+export const DarkTheme = {
+    body: darkContent,
+    inside: backgroundDark ,
+    text: txtBackgroundDark
+}
