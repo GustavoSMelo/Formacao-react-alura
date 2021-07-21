19c5ca3a2aad9b1d1c259abca321d11f27a69ef3
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-20 22:09:19 -0300

    add theme provider

--- react04/src/Components/Container/index.jsx
+++ react04/src/Components/Container/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ContainerMain } from './styled'
 import { ThemeProvider } from 'styled-components'
-import { DarkTheme, LightTheme } from '../../themes/themes'
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 import Extrato from "../Extrato";
